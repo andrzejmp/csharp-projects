5362c99c72fef852f1ea0b5e66247e38b462f066
andrzejmp <itstuff@ematmac.pl>
2019-03-21 12:46:20 +0100

    version 01

---
+++ databases/prog01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace sqlserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.\\SQLEXPRESS;Database=NBA;Trusted_Connection=Yes";
+            SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "select * from gracz;";
+            try
+            {
+                conn.Open();
+                SqlDataReader rdr = null;
+            
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr["idGracza"].ToString() + " " + rdr["nazwisko"].ToString() + " " + rdr["imie"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            conn.Close();
+            //Console.ReadKey();
+        }
+    }
+}
